856d06e41ad45e162e1daf798967cac98aa9ab51
Vartika Singh <vartikasinghv37@gmail.com>
2026-06-15 22:33:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (44.8%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num == 0) return "0";
+
+        bool negative = num < 0;
+        num = abs(num);
+
+        string result = "";
+
+        while (num > 0) {
+            result += (num % 7) + '0';
+            num /= 7;
+        }
+
+        reverse(result.begin(), result.end());
+
+        if (negative) {
+            result = "-" + result;
+        }
+
+        return result;
+    }
+};
